b3908716ae16e1e3ae899893015fa39504d621fa
Andre Francelino <andrefgomes.github.version@gmail.com>
2024-02-18 11:59:06 -0300

    Descrição das etapas do desafio

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,11 @@
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        /* 
+            Desafio Conta Bancária - trilha Java Básico - DIO
+                * Etapa 1 - Conhecer e importar a classe Scanner
+                * Etapa 2 - Exibir mensagens para o usuário inserir valores
+                * Etapa 3 - Obter via Scanner os valores digitados pelo usuário
+                * Etapa 4 - Exibir para o usuário mensagem com dados da conta criada
+        */
     }
 }
